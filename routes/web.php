ed98aa18e2cce6f85ea1380e34b1b9beebab9a95
Eyj1n <reijin.musa@usant.edu.ph>
2024-05-10 08:27:52 +0800

    act7_musa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
